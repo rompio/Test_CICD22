339ccc3041529c2b58f11c1ddd101e1d328a4f9e
ramon <ramonpolanski@gmail.com>
2024-10-22 13:25:51 +0200

    added count and no-items tests

--- shop/tests.py
+++ shop/tests.py
@@ -2,15 +2,26 @@ from django.test import TestCase
 from shop.models import Item
 from django.urls import reverse
 
-# Create your tests here.
+
 class ItemTest(TestCase):
     def setUp(self):
         self.item = Item.objects.create(
-            title = 'test item',
-            description = 'test description',
-            stock = 3
+            title='test item',
+            description='test description',
+            stock=3
         )
+
     def test_items_list(self):
         response = self.client.get(reverse('shop:index'))
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'test item')
+
+    def test_item_count(self):
+        response = self.client.get(reverse('shop:index'))
+        self.assertEqual(len(response.context['items']), 1)
+
+    def test_no_items(self):
+        Item.objects.all().delete() 
+        response = self.client.get(reverse('shop:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No items available')  
